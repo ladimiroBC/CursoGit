b0df6febbfe97e7349f051a0d2abc4352007335a
ladimiro <ladimiro1983@hotmail.com>
2022-02-03 21:18:59 -0500

    iniciando formulario

---
+++ formulario_aceptado/codigo_form.js
@@ -0,0 +1,9 @@
+alert("probando")
+
+let nombre=prompt("Digita tu nombre y luego haz click en el botom: ");
+
+const comenzar=(nom)=>{
+    alert("Bienvenido "+nom+" comienza a diligenciar el form");
+}
+
+comenzar(nombre);
